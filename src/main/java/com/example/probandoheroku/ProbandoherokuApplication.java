174a2211a9ab42aec9f18e995bbec8df303c5cc6
Vanesa Curci <curcivanesa@gmail.com>
2022-03-27 12:07:11 -0300

    first commit with project base

---
+++ src/main/java/com/example/probandoheroku/ProbandoherokuApplication.java
@@ -0,0 +1,13 @@
+package com.example.probandoheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbandoherokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProbandoherokuApplication.class, args);
+	}
+
+}
